f9cee40f728b941cb3a9887747f260f6ad47e2ee
Sam White <samwhiteiv@gmail.com>
2020-12-09 15:18:41 -0600

    update instances of var/let variable declarations to let/const

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,7 +1,7 @@
 // PROVIDED CODE BELOW (LINES 1 - 80) DO NOT REMOVE
 
 // The store will hold all information needed globally
-var store = {
+let store = {
 	track_id: undefined,
 	player_id: undefined,
 	race_id: undefined,
@@ -312,7 +312,7 @@ function resultsView(positions) {
 }
 
 function raceProgress(positions) {
-	let userPlayer = positions.find(e => e.id === store.player_id)
+	const userPlayer = positions.find(e => e.id === store.player_id)
 	userPlayer.driver_name += " (you)"
 
 	positions = positions.sort((a, b) => (a.segment > b.segment) ? -1 : 1)
